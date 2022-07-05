182c19d01e2986a0907bf80315aef7a89d07d51b
sebek78 <sebasporek@gmail.com>
2022-07-05 20:13:30 +0200

    Serve static file

--- webserver.ts
+++ webserver.ts
@@ -1,15 +1,35 @@
-// deno run --allow-net webserver.ts
-
+// deno run --allow-net --allow-read --watch webserver.ts
 import { serve } from "https://deno.land/std@0.146.0/http/server.ts";
-
 const port = 8080;
 
-const handler = (request: Request): Response => {
-  const body = `Your user-agent is:\n\n${
-    request.headers.get("user-agent") ?? "Unknown"
-  }`;
+const handler = async (request: Request): Promise<Response> => {
+  const { pathname } = new URL(request.url);
 
-  return new Response(body, { status: 200 });
+  if (pathname.startsWith("/style.css")) {
+    const file = await Deno.readFile("./style.css");
+
+    return new Response(file, {
+      headers: {
+        "content-type": "text/css",
+      },
+    });
+  }
+
+  return new Response(
+    `<html>
+      <head>
+        <link rel="stylesheet" href="style.css" />
+      </head>
+      <body>
+        <h1>Example</h1>
+      </body>
+    </html>`,
+    {
+      headers: {
+        "content-type": "text/html; charset=utf-8",
+      },
+    }
+  );
 };
 
 console.log(`HTTP webserver running. Access it at: http://localhost:8080/`);
